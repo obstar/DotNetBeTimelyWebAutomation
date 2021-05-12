3d3aec28c7cec3ef049690ddd6a87c96e32b9f38
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-12 22:10:27 +0200

    added new class UserInfo.cs

---
+++ DotNetBeTimelyWebAutomation/Context/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetBeTimelyWebAutomation.Tests.Context
+{
+    public class UserInfo
+    {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+    }
+}
