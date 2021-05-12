0aea61651829e598284a68e07f793f7d55ccc4fd
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-12 22:11:43 +0200

    added new methods to SignUpPageSteps.cs

--- DotNetBeTimelyWebAutomation/PageSteps/SignUpPageSteps.cs
+++ DotNetBeTimelyWebAutomation/PageSteps/SignUpPageSteps.cs
@@ -1,4 +1,5 @@
-﻿using DotNetBeTimelyWebAutomation.Tests.PageActions;
+﻿using System.Threading;
+using DotNetBeTimelyWebAutomation.Tests.PageActions;
 using TechTalk.SpecFlow;
 
 namespace DotNetBeTimelyWebAutomation.Tests.PageSteps
@@ -19,6 +20,19 @@ namespace DotNetBeTimelyWebAutomation.Tests.PageSteps
         public void GivenIGoToStagingTimelySignUpPage()
         {
             _signUpPageActions.NavigateToPage();
+        }
+
+        [When(@"I enter my custom details on Sign Up page")]
+        public void WhenIEnterMyCustomDetailsOnSignUpPage()
+        {
+            _signUpPageActions.FillInSignUpForm();
+        }
+
+        [When(@"I click Start free 14 day trial button on Sign Up page")]
+        public void WhenIClickStartFreeDayTrialButtonOnSignUpPage()
+        {
+            _signUpPageActions.ClickButtonStartFreeDayTrial();
+            Thread.Sleep(5000);
         }
 
         [Then(@"user can see Sign Up page")]
