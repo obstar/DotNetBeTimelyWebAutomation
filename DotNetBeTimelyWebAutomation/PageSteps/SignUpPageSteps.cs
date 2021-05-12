fedc3747936b3f7a56ca5b7fdef18f4a420cb34b
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-12 21:01:32 +0200

    added new class SignUpPageSteps.cs

---
+++ DotNetBeTimelyWebAutomation/PageSteps/SignUpPageSteps.cs
@@ -0,0 +1,29 @@
+﻿using TechTalk.SpecFlow;
+
+namespace DotNetBeTimelyWebAutomation.Tests.PageSteps
+{
+    [Binding]
+    [Scope(Feature = "SignUp")]
+    [Scope(Tag = "Register")]
+    public class SignUpPageSteps
+    {
+        private SignUpPageActions _signUpPageActions;
+
+        public SignUpPageSteps(SignUpPageActions signUpPageActions)
+        {
+            _signUpPageActions = signUpPageActions;
+        }
+
+        [Given(@"I go to staging timely Sign up page")]
+        public void GivenIGoToStagingTimelySignUpPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"user can see Sign Up page")]
+        public void ThenUserCanSeeSignUpPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
