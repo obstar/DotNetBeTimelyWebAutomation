426abf733482ba170ff10166163a79b1fc27cf12
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-12 21:04:16 +0200

    added new class SignUpPage.cs

---
+++ DotNetBeTimelyWebAutomation/PageObjects/SignUpPage.cs
@@ -0,0 +1,16 @@
+﻿namespace DotNetBeTimelyWebAutomation.Tests.PageObjects
+{
+    public class SignUpPage
+    {
+        public static string Url { get; } = "https://app.betimely.com/join";
+
+        public static string ButtonCssStartFreeDayTrial { get; } = "div.create-account";
+        public static string ButtonTextSignUpWithApple { get; } = "Sign up with Google";
+        public static string ButtonTextSignUpWithGoogle { get; } = "Sign up with Apple";
+        public static string InputIdFullName { get; } = "name";
+        public static string InputIdPassword { get; } = "password";
+        public static string InputIdWorkEmail { get; } = "email";
+        public static string LinkTextLogIn { get; } = "Log In";
+        public static string LinkTextTermsOfService { get; } = "Terms of Service.";
+    }
+}
