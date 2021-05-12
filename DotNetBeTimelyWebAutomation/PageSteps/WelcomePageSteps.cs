c6b8c53091b22315ab5c0a795a9c57d2448d4cae
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-12 22:13:21 +0200

    added new class WelcomePageSteps.cs

---
+++ DotNetBeTimelyWebAutomation/PageSteps/WelcomePageSteps.cs
@@ -0,0 +1,22 @@
+﻿using TechTalk.SpecFlow;
+
+namespace DotNetBeTimelyWebAutomation.Tests.PageSteps
+{
+    [Binding]
+    [Scope(Feature = "SignUp")]
+    public class WelcomePageSteps
+    {
+        private readonly WelcomePageActions _welcomePageStepsPageActions;
+
+        public WelcomePageSteps(WelcomePageActions welcomePageStepsPageActions)
+        {
+            _welcomePageStepsPageActions = welcomePageStepsPageActions;
+        }
+
+        [Then(@"user can see Welcome page")]
+        public void ThenUserCanSeeWelcomePage()
+        {
+            _welcomePageStepsPageActions.AssertPageLoaded();
+        }
+    }
+}
