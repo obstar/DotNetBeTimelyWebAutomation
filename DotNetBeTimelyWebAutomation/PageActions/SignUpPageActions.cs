f9bb4b61548f6e4084679bc301fd004367a5ac8f
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-12 22:12:06 +0200

    added new methods to SignUpPageActions.cs class

--- DotNetBeTimelyWebAutomation/PageActions/SignUpPageActions.cs
+++ DotNetBeTimelyWebAutomation/PageActions/SignUpPageActions.cs
@@ -1,5 +1,8 @@
-﻿using Coypu;
+﻿using System;
+using Coypu;
+using DotNetBeTimelyWebAutomation.Tests.Context;
 using DotNetBeTimelyWebAutomation.Tests.PageObjects;
+using DotNetBeTimelyWebAutomation.Tests.Support;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 
@@ -9,10 +12,16 @@ namespace DotNetBeTimelyWebAutomation.Tests.PageActions
     public class SignUpPageActions
     {
         private readonly BrowserSession _browser;
+        private readonly FormDataGenerator _formDataGenerator;
+        private readonly SessionContext _sessionContext;
 
-        public SignUpPageActions(BrowserSession browser)
+        public SignUpPageActions(BrowserSession browser,
+                                 FormDataGenerator formDataGenerator,
+                                 SessionContext sessionContext)
         {
             _browser = browser;
+            _formDataGenerator = formDataGenerator;
+            _sessionContext = sessionContext;
         }
 
         public void NavigateToPage()
@@ -23,14 +32,59 @@ namespace DotNetBeTimelyWebAutomation.Tests.PageActions
         public void AssertPageLoaded()
         {
             Assert.AreEqual("Register your Timely account - Timely", _browser.Title);
-            Assert.IsTrue(_browser.FindLink(SignUpPage.LinkTextLogIn).Exists());
-            Assert.IsTrue(_browser.FindButton(SignUpPage.ButtonTextSignUpWithGoogle).Exists());
-            Assert.IsTrue(_browser.FindButton(SignUpPage.ButtonTextSignUpWithApple).Exists());
-            Assert.IsTrue(_browser.FindId(SignUpPage.InputIdWorkEmail).Exists());
-            Assert.IsTrue(_browser.FindId(SignUpPage.InputIdFullName).Exists());
-            Assert.IsTrue(_browser.FindId(SignUpPage.InputIdPassword).Exists());
-            Assert.IsTrue(_browser.FindCss(SignUpPage.ButtonCssStartFreeDayTrial).Exists());
-            Assert.IsTrue(_browser.FindLink(SignUpPage.LinkTextTermsOfService).Exists());
+            Assert.IsTrue(_browser.FindLink(SignUpPage.LinkTextLogIn)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindButton(SignUpPage.ButtonTextSignUpWithGoogle)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindButton(SignUpPage.ButtonTextSignUpWithApple)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindId(SignUpPage.InputIdWorkEmail)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindId(SignUpPage.InputIdFullName)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindId(SignUpPage.InputIdPassword)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindCss(SignUpPage.ButtonCssStartFreeDayTrial)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindLink(SignUpPage.LinkTextTermsOfService)
+                                  .Exists());
+        }
+
+        public void ClickButtonStartFreeDayTrial()
+        {
+            _browser.FindCss(SignUpPage.ButtonCssStartFreeDayTrial)
+                    .Click();
+        }
+
+        public void FillInSignUpForm()
+        {
+            FillInWorkEmail($"jakub{_formDataGenerator.AnAlphaNumericCode(5)}@memorytest.ai");
+            FillInFullName($"Jakub {_formDataGenerator.AnAlphaCode(5)}");
+            FillInPassword($"password{_formDataGenerator.AnAlphaNumericCode(5)}");
+        }
+
+        private void FillInWorkEmail(string workEmail)
+        {
+            Console.WriteLine($"\t -> Work email: {workEmail}");
+            _browser.FindId(SignUpPage.InputIdWorkEmail)
+                    .FillInWith(workEmail);
+            _sessionContext.User.Email = workEmail;
+        }
+
+        private void FillInFullName(string fullName)
+        {
+            Console.WriteLine($"\t -> Full Name: {fullName}");
+            _browser.FindId(SignUpPage.InputIdFullName)
+                    .FillInWith(fullName);
+            _sessionContext.User.FullName = fullName;
+        }
+
+        private void FillInPassword(string password)
+        {
+            Console.WriteLine($"\t -> Password: {password}");
+            _browser.FindId(SignUpPage.InputIdPassword)
+                    .FillInWith(password);
+            _sessionContext.User.Password = password;
         }
     }
 }
