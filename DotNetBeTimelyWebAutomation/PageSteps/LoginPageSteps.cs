57f33f90ea70c1185fbd91f7df1a981ed77cbdde
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-13 13:09:55 +0200

    added new class LoginPageSteps.cs

---
+++ DotNetBeTimelyWebAutomation/PageSteps/LoginPageSteps.cs
@@ -0,0 +1,23 @@
+﻿using DotNetBeTimelyWebAutomation.Tests.PageActions;
+using TechTalk.SpecFlow;
+
+namespace DotNetBeTimelyWebAutomation.Tests.PageSteps
+{
+    [Binding]
+    [Scope(Feature = "SignUpLinks")]
+    public class LoginPageSteps
+    {
+        private readonly LoginPageActions _loginPageActions;
+
+        public LoginPageSteps(LoginPageActions loginPageActions)
+        {
+            _loginPageActions = loginPageActions;
+        }
+
+        [Then(@"user can see Login page")]
+        public void ThenUserCanSeeLoginPage()
+        {
+            _loginPageActions.AssertPageLoaded();
+        }
+    }
+}
