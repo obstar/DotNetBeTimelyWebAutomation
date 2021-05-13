97891651cb61daaf3057ed2f2be56c84c05066bf
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-13 13:32:45 +0200

    added new class TermsOfServicePageSteps.cs

---
+++ DotNetBeTimelyWebAutomation/PageSteps/TermsOfServicePageSteps.cs
@@ -0,0 +1,23 @@
+﻿using DotNetBeTimelyWebAutomation.Tests.PageActions;
+using TechTalk.SpecFlow;
+
+namespace DotNetBeTimelyWebAutomation.Tests.PageSteps
+{
+    [Binding]
+    [Scope(Feature = "SignUpLinks")]
+    public class TermsOfServicePageSteps
+    {
+        private readonly TermsOfServicePageActions _termsOfServicePageActions;
+
+        public TermsOfServicePageSteps(TermsOfServicePageActions termsOfServicePageActions)
+        {
+            _termsOfServicePageActions = termsOfServicePageActions;
+        }
+
+        [Then(@"user can see Terms Of Service page")]
+        public void ThenUserCanSeeTermsOfServicePage()
+        {
+            _termsOfServicePageActions.AssertPageLoaded();
+        }
+    }
+}
