d8cd09601855a06a4737dbaf84c04d3ffc7bf0d3
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-13 13:09:34 +0200

    added new class LoginPageActions.cs

---
+++ DotNetBeTimelyWebAutomation/PageActions/LoginPageActions.cs
@@ -0,0 +1,37 @@
+﻿using Coypu;
+using DotNetBeTimelyWebAutomation.Tests.PageObjects;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace DotNetBeTimelyWebAutomation.Tests.PageActions
+{
+    [Binding]
+    public class LoginPageActions
+    {
+        private readonly BrowserSession _browser;
+
+        public LoginPageActions(BrowserSession browser)
+        {
+            _browser = browser;
+        }
+
+        public void AssertPageLoaded()
+        {
+            Assert.IsTrue(_browser.FindCss(LoginPage.LinkCssSignUp)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindId(LoginPage.InputIdEmail)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindId(LoginPage.ButtonIdNext)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindLink(LoginPage.LinkTextIForgotMyPassword)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindButton(LoginPage.ButtonTextSignInWithGoogle)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindButton(LoginPage.ButtonTextSignInWithApple)
+                                  .Exists());
+            Assert.IsTrue(_browser.FindLink(LoginPage.LinkTextReadMore)
+                                  .Exists());
+            Assert.AreEqual(LoginPage.Title, _browser.Title);
+        }
+    }
+}
