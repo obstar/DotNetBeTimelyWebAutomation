841ece520f88266d7dac951550a18d2da171c93e
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-13 13:32:30 +0200

    added new class TermsOfServicePageActions.cs

---
+++ DotNetBeTimelyWebAutomation/PageActions/TermsOfServicePageActions.cs
@@ -0,0 +1,29 @@
+﻿using Coypu;
+using DotNetBeTimelyWebAutomation.Tests.PageObjects;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace DotNetBeTimelyWebAutomation.Tests.PageActions
+{
+    [Binding]
+    public class TermsOfServicePageActions
+    {
+        private readonly BrowserSession _browser;
+
+        public TermsOfServicePageActions(BrowserSession browser)
+        {
+            _browser = browser;
+        }
+
+        public void AssertPageLoaded()
+        {
+            var termsOfServiceWindow = _browser.FindWindow(TermsOfServicePage.Title);
+            Assert.IsTrue(termsOfServiceWindow.FindCss(TermsOfServicePage.DivCssMenu)
+                                              .Exists());
+            Assert.IsTrue(termsOfServiceWindow.FindCss(TermsOfServicePage.DivCssTermsOfServiceText)
+                                              .Exists());
+            StringAssert.Contains(TermsOfServicePage.Subtitle, termsOfServiceWindow.FindCss(TermsOfServicePage.DivCssTermsOfServiceText).Text);
+            Assert.AreEqual(TermsOfServicePage.Title, termsOfServiceWindow.Title);
+        }
+    }
+}
