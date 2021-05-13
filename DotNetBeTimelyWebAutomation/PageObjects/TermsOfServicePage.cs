db3abedfdd3747cc6f8ea0ddc8f1df36e84fda32
Jakub Obstarczyk <jakub.obstarczyk@gmail.com>
2021-05-13 13:32:13 +0200

    added new class TermsOfServicePage.cs

---
+++ DotNetBeTimelyWebAutomation/PageObjects/TermsOfServicePage.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetBeTimelyWebAutomation.Tests.PageObjects
+{
+    public class TermsOfServicePage
+    {
+        public static string DivCssMenu { get; } = "div.menu-text-light";
+        public static string DivCssTermsOfServiceText { get; } = "div.privacy-policy-rich-text.w-richtext";
+        public static string Subtitle { get; } = "Memory AS Terms of Service";
+        public static string Title { get; } = "Terms of Service - Memory";
+        public static string Url { get; } = "https://memory.ai/terms-of-service";
+    }
+}
